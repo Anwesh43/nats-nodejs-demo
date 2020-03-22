91c248f89156eab76988a6a5fdeb61cc8eb7039f
Anwesh43 <anweshthecool0@gmail.com>
2020-03-23 00:28:59 +0530

    adding publisher

--- publisher.js
+++ publisher.js
@@ -1,9 +1,20 @@
 const NATS = require('nats')
 const nc = NATS.connect()
+const {SUBJECT_NAME, STOP_SUBSCRIBER_MESSAGE} = require('./constants')
 console.log("connected")
 console.log("closing in 3 seconds")
-setTimeout(() => {
-    console.log("closing nc")
-    nc.close()
-    console.log("closed")
-}, 3000)
+
+let i = 0
+console.log("sending messages")
+const interval = setInterval(() => {
+    nc.publish(SUBJECT_NAME, `message${i++}`)
+    if (i == 100) {
+        clearInterval(interval)
+        nc.publish(SUBJECT_NAME, STOP_SUBSCRIBER_MESSAGE)
+        setTimeout(() => {
+            console.log("finished sending messages")
+            nc.close()
+        }, 2000)
+
+    }
+}, 1000)
